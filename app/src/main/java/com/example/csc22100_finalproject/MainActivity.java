8a772e5bff5151121f8aab44744bf8f2146723d1
SyedTDK <74115865+SyedTDK@users.noreply.github.com>
2022-05-24 16:21:54 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/csc22100_finalproject/MainActivity.java
+++ app/src/main/java/com/example/csc22100_finalproject/MainActivity.java
@@ -29,32 +29,13 @@ public class MainActivity extends AppCompatActivity {
         tabs.setupWithViewPager(viewPager);
 
         Adapter adapter = new Adapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
-        adapter.addFragment(new fragment1(), "Chest");
+        adapter.addFragment(new fragment1(), "Chest");   //For every tab
         adapter.addFragment(new fragment2(), "Back");
         adapter.addFragment(new fragment3(), "Arms");
         adapter.addFragment(new fragment4(), "Legs");
         adapter.addFragment(new fragment5(), "Shoulders");
         adapter.addFragment(new fragment6(), "Abs" );
         viewPager.setAdapter(adapter);
-
-/*        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = binding.viewpager;
-        viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = binding.tabs;
-        tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = binding.fab;
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        }); */
-
 
     }
 }
